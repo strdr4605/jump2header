09599c105ff4e7d9272a171cd9268a6784895ef9
Dragoș Străinu <str.dr4605@gmail.com>
2020-04-04 13:29:39 +0300

    fix: MARKDOWN_ANCHOR_LINK_REGEXP issue

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const ARROW_UP = "\u2B06";
 
 export const WHITE_SPASE_REGEXP = /\s/g;
-export const MARKDOWN_ANCHOR_LINK_REGEXP = /\[.+\]\(\#.+\)$/g;
+export const MARKDOWN_ANCHOR_LINK_REGEXP = /\[.+\]\(\#.+\)$/;
 export const MARKDOWN_ANY_LINK_REGEXP = /\[.+\]\(.+\)$/g;
 export const MARKDOWN_HEADER_REGEXP = /^\#{1,6}\s+/;
 export const SPICIAL_CHARS_REGEXP = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~’]/g;
